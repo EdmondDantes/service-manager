e7655671805c33bdca05bab5aee1c58e8a138f97
Edmond <1571649+EdmondDantes@users.noreply.github.com>
2024-08-04 19:47:55 +0300

    + DescriptorWalker

---
+++ src/DescriptorWalker.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace IfCastle\ServiceManager;
+
+final class DescriptorWalker
+{
+    public static function walk(DescriptorRepositoryInterface $descriptorRepository): iterable
+    {
+        foreach ($descriptorRepository->getServiceDescriptorList() as $serviceDescriptor) {
+            foreach ($serviceDescriptor->getServiceMethods() as $serviceMethod) {
+                $isBreak = yield $serviceDescriptor->getServiceName() => $serviceMethod;
+
+                if(true === $isBreak) {
+                    return;
+                }
+            }
+        }
+    }
+}
