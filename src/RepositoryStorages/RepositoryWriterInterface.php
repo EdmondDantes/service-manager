2569eb63718f20454a4d0a15ffcc8c8e9096ce07
Edmond <1571649+EdmondDantes@users.noreply.github.com>
2024-10-25 13:28:58 +0300

    % refactoring RepositoryWriterInterface

--- src/RepositoryStorages/RepositoryWriterInterface.php
+++ src/RepositoryStorages/RepositoryWriterInterface.php
@@ -5,7 +5,12 @@ namespace IfCastle\ServiceManager\RepositoryStorages;
 
 interface RepositoryWriterInterface extends RepositoryReaderInterface
 {
-    public function addServiceConfig(string $serviceName, array $serviceConfig, array|null $includeTags = null, array|null $excludeTags = null): void;
+    public function addServiceConfig(string     $serviceName,
+                                     array      $serviceConfig,
+                                     bool       $isActive = true,
+                                     array|null $includeTags = null,
+                                     array|null $excludeTags = null
+    ): void;
     
     public function removeServiceConfig(string $serviceName): void;
     
