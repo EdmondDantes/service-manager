fbfce80cb328655147dbe04e659ffd395f4d588a
Edmond <1571649+EdmondDantes@users.noreply.github.com>
2024-10-30 14:30:47 +0200

    + RepositoryWriterInterface add serviceSuffix

--- src/RepositoryStorages/RepositoryWriterInterface.php
+++ src/RepositoryStorages/RepositoryWriterInterface.php
@@ -4,24 +4,37 @@ declare(strict_types=1);
 
 namespace IfCastle\ServiceManager\RepositoryStorages;
 
+/**
+ * From the consumer's perspective, the service name should be a unique key that unequivocally identifies the service.
+ * However, from the system's perspective,
+ * a single name may correspond to multiple services depending on the scope/execution environment.
+ *
+ * Therefore, the service repository uses both the `package name` and the `service name` to uniquely identify a service.
+ *
+ * If a package defines MULTIPLE services, it then uses the `$serviceSuffix` parameter.
+ */
 interface RepositoryWriterInterface extends RepositoryReaderInterface, RepositoryReaderByTagsInterface
 {
     /**
      * Adds a new service configuration to the repository.
+     *
+     * If $serviceSuffix is not provided, the service is considered to be the default service.
      *
      * @param array<string, mixed> $serviceConfig
      * @param ?array<string>        $includeTags
      * @param ?array<string>        $excludeTags
      */
     public function addServiceConfig(
+        string     $packageName,
         string     $serviceName,
         array      $serviceConfig,
         bool       $isActive = true,
         array|null $includeTags = null,
-        array|null $excludeTags = null
+        array|null $excludeTags = null,
+        string|null $serviceSuffix = null
     ): void;
 
-    public function removeServiceConfig(string $serviceName): void;
+    public function removeServiceConfig(string $packageName, string $serviceName): void;
 
     /**
      * Updates the service configuration in the repository.
@@ -30,15 +43,17 @@ interface RepositoryWriterInterface extends RepositoryReaderInterface, Repositor
      * @param ?array<string>        $includeTags
      * @param ?array<string>        $excludeTags
      */
-    public function updateServiceConfig(string     $serviceName,
+    public function updateServiceConfig(string     $packageName,
+                                        string     $serviceName,
                                         array      $serviceConfig,
                                         array|null $includeTags = null,
-                                        array|null $excludeTags = null
+                                        array|null $excludeTags = null,
+                                        string|null $serviceSuffix = null
     ): void;
 
-    public function activateService(string $serviceName): void;
+    public function activateService(string $packageName, string $serviceName, string $serviceSuffix): void;
 
-    public function deactivateService(string $serviceName): void;
+    public function deactivateService(string $packageName, string $serviceName, string $serviceSuffix): void;
 
     /**
      * Changes the tags of a service.
@@ -46,7 +61,10 @@ interface RepositoryWriterInterface extends RepositoryReaderInterface, Repositor
      * @param ?array<string> $includeTags
      * @param ?array<string> $excludeTags
      */
-    public function changeServiceTags(string     $serviceName,
+    public function changeServiceTags(
+                                      string $packageName,
+                                      string $serviceName,
+                                      string $serviceSuffix,
                                       array|null $includeTags = null,
                                       array|null $excludeTags = null
     ): void;
