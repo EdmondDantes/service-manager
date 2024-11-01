32715e580dd069fabcaf42c378e28a27e8201534
Edmond <1571649+EdmondDantes@users.noreply.github.com>
2024-11-01 13:38:13 +0200

    + docs for WorkerExecutorInterface.php

--- src/WorkerExecutorInterface.php
+++ src/WorkerExecutorInterface.php
@@ -4,6 +4,12 @@ declare(strict_types=1);
 
 namespace IfCastle\ServiceManager;
 
+/**
+ * ## WorkerExecutorInterface
+ *
+ * This interface provides the ability to execute commands in worker processes.
+ * It differs from the Command-bus interface of ExecutorInterface by explicitly specifying the execution method.
+ */
 interface WorkerExecutorInterface
 {
     /**
