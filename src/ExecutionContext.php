372f21dff7573a9638136c556b9b89261da90d53
Edmond <1571649+EdmondDantes@users.noreply.github.com>
2024-11-08 13:53:29 +0200

    + ExecutionContext

--- src/ExecutionContext.php
+++ src/ExecutionContext.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace IfCastle\ServiceManager;
@@ -6,79 +7,32 @@ namespace IfCastle\ServiceManager;
 use IfCastle\DesignPatterns\KeyValueContext\KeyValueContext;
 use IfCastle\Exceptions\SerializeException;
 use IfCastle\Exceptions\UnSerializeException;
-use IfCastle\TypeDefinitions\NativeSerialization\ArraySerializableInterface;
+use IfCastle\TypeDefinitions\NativeSerialization\ArraySerializableTrait;
 use IfCastle\TypeDefinitions\NativeSerialization\ArraySerializableValidatorInterface;
 
 class ExecutionContext extends KeyValueContext implements ExecutionContextInterface
 {
+    use ArraySerializableTrait;
+
     /**
      * @throws SerializeException
      */
     #[\Override]
     public function toArray(?ArraySerializableValidatorInterface $validator = null): array
     {
-        if($validator?->isSerializationAllowed($this) === false) {
+        if ($validator?->isSerializationAllowed($this) === false) {
             throw new SerializeException('Serialization is not allowed', $this, 'array', $this);
         }
-        
-        $result = [];
-        
-        foreach ($this->context as $key => $value) {
-            if ($value instanceof ArraySerializableInterface) {
-                $result[$key] = $value->toArray($validator);
-            } else if(is_scalar($value) || is_null($value)) {
-                $result[$key] = $value;
-            } else {
-                throw new SerializeException('The value of the context should be scalar', $value, 'array', $this);
-            }
-        }
-        
-        return $result;
+
+        return $this->serializeToArray($this->context, $validator);
     }
-    
+
     /**
      * @throws UnSerializeException
      */
     #[\Override]
     public static function fromArray(array $array, ?ArraySerializableValidatorInterface $validator = null): static
     {
-        $context                    = [];
-        $stack                      = [];
-        $iterator                   = new \ArrayIterator($array);
-        $iterator->rewind();
-        
-        while (true) {
-            
-            while ($iterator->valid()) {
-                $value              = $iterator->current();
-                $key                = $iterator->key();
-                
-                if(is_array($value)) {
-                    
-                    $iterator->next();
-                    $context[$key]  = [];
-                    $stack[]        = [$iterator, &$context];
-                    
-                    $context        = &$context[$key];
-                    $iterator       = new \ArrayIterator($value);
-                    $iterator->rewind();
-                    
-                    break;
-                } else if(is_scalar($value) || is_null($value)) {
-                    $context[$key]  = $value;
-                    $iterator->next();
-                } else {
-                    throw new UnSerializeException('The value of the context should be scalar', 'array', $value);
-                }
-            }
-
-            if($stack === []) {
-                break;
-            }
-            
-            [$iterator, &$context]   = array_pop($stack);
-        }
-        
-        return new static($context);
+        return new static(static::unserializeFromArray($array));
     }
 }
